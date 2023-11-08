f1986a275779f431068f6151631b8c96681e19a6
Johan Pachon <sebastian-johan1@hotmail.com>
2023-11-08 07:34:05 -0500

    feat: :sparkles: Added lastest changes

--- querys.sql
+++ querys.sql
@@ -39,8 +39,36 @@ JOIN oficina o ON e.codigo_oficina = o.codigo_oficina
 WHERE p.codigo_cliente IS NULL;
 
 -- 6 
-
 SELECT CONCAT(o.linea_direccion1,', ',o.linea_direccion2) as Direccion
 FROM oficina o
 JOIN empleado e ON e.codigo_oficina = o.codigo_oficina
-JOIN cliente c ON e.codigo_empleado = c.codigo_empleado_rep_ventas;
+JOIN cliente c ON e.codigo_empleado = c.codigo_empleado_rep_ventas
+WHERE c.ciudad = 'Fuenlabrada';
+
+-- 7
+SELECT c.nombre_cliente as Cliente, CONCAT(e.nombre,' ',e.apellido1) as Representante, o.ciudad as Oficina
+FROM cliente c
+JOIN empleado e ON c.codigo_empleado_rep_ventas = e.codigo_empleado
+JOIN oficina o ON e.codigo_oficina = o.codigo_oficina;
+
+-- 8
+SELECT CONCAT(e.nombre, ' ',e.apellido1) as Empleado, CONCAT(j.nombre,' ',j.apellido1) AS Jefe
+FROM empleado e
+JOIN empleado j ON e.codigo_jefe = j.codigo_empleado;
+
+-- 9
+SELECT CONCAT(e.nombre, ' ',e.apellido1) as Empleado, CONCAT(j.nombre,' ',j.apellido1) AS Jefe, CONCAT(s.nombre,' ',s.apellido1) AS Superior
+FROM empleado e
+JOIN empleado j ON e.codigo_jefe = j.codigo_empleado
+JOIN empleado s ON j.codigo_jefe = s.codigo_empleado;
+
+-- 10
+SELECT c.nombre_cliente as Cliente, GROUP_CONCAT(p.codigo_pedido) as Pedidos_Id
+FROM cliente c
+JOIN pedido p ON c.codigo_cliente = p.codigo_cliente
+WHERE p.fecha_esperada = p.fecha_entrega
+GROUP BY c.nombre_cliente;
+
+SELECT c.nombre_cliente as Cliente, GROUP_CONCAT(c.nombre_cliente) as Gamas
+FROM cliente c
+JOIN pedido p ON c.codigo_cliente
