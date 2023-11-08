a1c88a8629a46381e892911903744b953885edb6
johanp0308 <sebastian-johan1@hotmail.com>
2023-11-07 23:49:35 -0500

    feat: :sparkles: Adding queries

--- querys.sql
+++ querys.sql
@@ -12,9 +12,35 @@ JOIN pago p on p.codigo_cliente = c.codigo_cliente;
 -- 3
 SELECT c.nombre_cliente as Cliente, c.codigo_cliente as Codigo
 FROM cliente c
-LEFT JOIN pago p on c.codigo_cliente = p.codigo_cliente;
+LEFT JOIN pago p on c.codigo_cliente = p.codigo_cliente
+WHERE p.codigo_cliente IS NULL
+;
 
 SELECT c.nombre_cliente as Cliente, c.codigo_cliente as Codigo
 FROM pago p
 RIGHT JOIN cliente c on c.codigo_cliente = p.codigo_cliente
-;
+WHERE p.codigo_cliente IS NULL;
+
+-- 4
+
+SELECT c.nombre_cliente as Cliente, e.nombre as Representante, o.ciudad as Ciudad
+FROM cliente c
+JOIN pago p ON c.codigo_cliente = p.codigo_cliente
+JOIN empleado e ON c.codigo_empleado_rep_ventas = e.codigo_empleado
+JOIN oficina o ON e.codigo_oficina = o.codigo_oficina; 
+
+-- 5
+
+SELECT c.nombre_cliente as Cliente, e.nombre as Representante, o.ciudad as Ciudad
+FROM cliente c
+LEFT JOIN pago p ON c.codigo_cliente = p.codigo_cliente
+JOIN empleado e ON c.codigo_empleado_rep_ventas = e.codigo_empleado
+JOIN oficina o ON e.codigo_oficina = o.codigo_oficina
+WHERE p.codigo_cliente IS NULL;
+
+-- 6 
+
+SELECT CONCAT(o.linea_direccion1,', ',o.linea_direccion2) as Direccion
+FROM oficina o
+JOIN empleado e ON e.codigo_oficina = o.codigo_oficina
+JOIN cliente c ON e.codigo_empleado = c.codigo_empleado_rep_ventas;
